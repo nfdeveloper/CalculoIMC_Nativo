bbff96de98b13d47e70c43f09ae93638244150a5
nfdeveloper <nfdeveloper04@gmail.com>
2024-09-18 11:59:38 -0300

    Criação de Projeto utilizando Android Studio para Calculo de IMC.

---
+++ app/src/main/java/com/nfdev/calculadorimc/ResultadoActivity.kt
@@ -0,0 +1,53 @@
+package com.nfdev.calculadorimc
+
+import android.os.Bundle
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class ResultadoActivity : AppCompatActivity() {
+
+    private lateinit var textPeso: TextView
+    private lateinit var textAltura: TextView
+    private lateinit var textResultado: TextView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_resultado)
+
+        textPeso = findViewById(R.id.text_peso)
+        textAltura = findViewById(R.id.text_altura)
+        textResultado = findViewById(R.id.text_resultado)
+
+        val bundle = intent.extras
+
+        if(bundle != null){
+            val peso = bundle.getDouble("peso")
+            val altura = bundle.getDouble("altura")
+
+            textPeso.text = "Peso informado $peso kg"
+            textAltura.text = "Altura informada $altura m"
+
+            val imc = peso / (altura * altura)
+
+            if(imc < 18.5){
+                textResultado.text = "Baixo"
+            } else if(imc in 18.5 .. 24.9) {
+                textResultado.text = "Normal"
+            } else if(imc in 25.0 .. 29.9){
+                textResultado.text = "Sobrepeso"
+            } else {
+                textResultado.text = "Obeso"
+            }
+        }
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
